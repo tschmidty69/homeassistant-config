b4a90fdb290d7ef9af8a2e06c098c0a7b4814bdb
Tod Schmidt <tschmidtY@yahoo.com>
2018-01-18 16:40:15 -0500

    Jarvis update

--- jarvis/jarvis_says.sh
+++ jarvis/jarvis_says.sh
@@ -1,42 +1,70 @@
 #!/bin/bash
 # Shell script to replace TTS in snips with AWS polly
 #
-# https://github.com/tschmidty69
-# adapted from
-# https://github.com/jvandewiel/
-#
-# Install and configure aws cli as per
-# https://docs.aws.amazon.com/polly/latest/dg/getting-started-cli.html
-# Installed in /home/<user>/.aws, configure with aws configure
-# and provide key, secret, etc. Best practice is to create an IAM
-# user and give tehm permission to just polly
-#
-# Edit the aws line below with your configure profile name and voice-ide prefered
+# Install and configure aws cli as per https://docs.aws.amazon.com/polly/latest/dg/getting-started-cli.html
+# Installed in /home/<user>/.local/bin, configure with aws configure and provide key, secret, etc.
 #
 # in /etc/snips.toml, change TTS config to contain following 3 lines
 # [snips-tts]
 # provider = "customtts"
-# customtts = { command = ["/usr/local/jarvis_says.sh", "-w", "%%OUTPUT_FILE%%", "-l", "%%LANG%%", "%%TEXT%%"] }
+# customtts = { command = ["/usr/local/bin/jarvis_says.sh", "-w", "%%OUTPUT_FILE%%", "-l", "%%LANG%%", "%%TEXT%%"] }
 #
 # install mpg123 (apt-get install mpg123) for the mp3->wav conversion
 #
-# This will run e.g. '"/usr/local/jarvis_says.sh" "-w" "/tmp/.tmpbQHj3W.wav" "-l" "en" "For how long?"'
-#
-output_file="$2"
-textstr="$5"
-# get interm filename
-interm_file="/tmp/sounds/$textstr.mp3"
-# debugging
-#echo 'Calling polly for output file:' $output_file >> /tmp/jarvis_says.log
-#echo 'and intermediate file:' $interm_file>> /tmp/jarvis_says.log
-#echo 'Input text:' $textstr >> /tmp/jarvis_says.log
-# execute polly to get mp3 - check paths, voice set to Salli
-mkdir -pv /tmp/sounds/
-if [ ! -f "$interm_file.wav" ]; then
-  aws --profile jarvis polly synthesize-speech --output-format mp3 --voice-id Geraint  --sample-rate 16000 --text "$textstr" "$interm_file"
-  # execute conversion to wav
-  #avconv -y -i "$interm_file" "$interm_file.wav"
-  mpg123 -w "$interm_file.wav" "$interm_file"
+# This will run e.g. '"/usr/local/bin/jarvis_says.sh" "-w" "/tmp/.tmpbQHj3W.wav" "-l" "en" "For how long?"'
+# 
+# Input text and parameters will be used to calculate a hash for caching the mp3 files so only
+# "new speech" will call polly, existing mp3s will be transformed in wav files directly
+
+# Folder to cache the files - this also contains the .txt file with all generated mp3
+cache="/tmp/cache/"
+
+# Path to aws binary
+awscli='/usr/local/bin/aws'
+
+# Voice to use
+voice="Geraint"
+
+# Should not need to change parameters below this
+# format to use
+format="mp3"
+
+# Sample rate to use
+samplerate="22050"
+
+# passed text string
+text="<speak>$5</speak>"
+echo 'Input text:' $text
+
+# target file to return to snips-tts (wav)
+outfile="$2"
+echo 'Output file:' $outfile 
+
+# check/create cache if needed
+mkdir -pv "$cache"
+
+# create hash for the string based on params and text
+md5string="$text""_""$voice""_""$format""_""$samplerate"
+echo 'Using string for hash': $md5string
+
+hash="$(echo -n "$md5string" | md5sum | sed 's/ .*$//')"
+echo 'Calculated hash:' $hash
+
+cachefile="$cache""$hash".mp3
+echo 'Cache file:' $cachefile 
+
+# do we have this?
+if [ -f "$cachefile" ]
+then
+    echo "$cachefile found."
+    # convert
+    mpg123 -w "$outfile" "$cachefile"
+else
+    echo "$cachefile not found, running polly"
+    # execute polly to get mp3 - check paths, voice set to Salli
+    $awscli --profile jarvis polly synthesize-speech --output-format "$format" --voice-id "$voice" --sample-rate "$samplerate" --text-type ssml --text "$text" "$cachefile"
+    # update index
+    echo "$hash" "$md5string" >> "$cache"index.txt
+    # execute conversion to wav
+    mpg123 -w $outfile $cachefile
 fi
-#echo "$interm_file.wav --> $output_file" >> /tmp/jarvis_says.log
-cp -v "$interm_file.wav" "$output_file"
